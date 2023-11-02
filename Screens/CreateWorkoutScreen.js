d120f2e399e5be175c341ff090234bfc5e031552
Andersson997 <olliver.lysekil@gmail.com>
2023-11-02 14:19:05 +0100

    before app icon

--- Screens/CreateWorkoutScreen.js
+++ Screens/CreateWorkoutScreen.js
@@ -11,23 +11,25 @@ import {
   KeyboardAvoidingView,
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import React, { useRef, useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { doc, getDoc, collection, setDoc } from "firebase/firestore";
 import { MuscleMagicAuth, MuscleMagicDb } from "../Database/FireBaseConfig";
 import ExerciseForm from "../Components/ExerciseForm";
 import { Dropdown } from "react-native-element-dropdown";
 import { LinearGradient } from "expo-linear-gradient";
-import "react-native-gesture-handler";
 import {
   BottomSheetModal,
   BottomSheetModalProvider,
   BottomSheetBackdrop,
 } from "@gorhom/bottom-sheet";
-function CreateWorkoutScreen({navigation}) {
+import "react-native-gesture-handler";
+
+function CreateWorkoutScreen({ navigation }) {
   const user = MuscleMagicAuth.currentUser;
   const db = MuscleMagicDb;
   const bottomSheetModalRef = useRef(null);
   const snapPoints = ["50%"];
+
   const data = [
     { label: "Monday", value: "1" },
     { label: "Tuesday", value: "2" },
@@ -131,102 +133,148 @@ function CreateWorkoutScreen({navigation}) {
         console.error("Error adding workout plan:", error);
       });
   };
-  
+
   return (
     <BottomSheetModalProvider>
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === "ios" ? "padding" : "undefined"}
-    >
-      <LinearGradient
-        style={styles.background}
-        colors={["#0F0264", "#0F3362", "#030D01"]}
-      />
-      <ScrollView style={styles.scrollview}>
-        <View style={styles.container}>
-          <View style={styles.formInputContainer}>
-            <TextInput
-              placeholder="Name"
-              placeholderTextColor="#C5C5CD"
-              style={styles.textInput}
-              value={workoutData.name}
-              autoCapitalize="none"
-              onChangeText={(text) =>
-                setWorkoutData((prevData) => ({ ...prevData, name: text }))
-              }
-            />
-            <TextInput
-              placeholder="Description"
-              placeholderTextColor="#C5C5CD"
-              style={styles.textInput}
-              value={workoutData.description}
-              autoCapitalize="none"
-              onChangeText={(text) =>
-                setWorkoutData((prevData) => ({
-                  ...prevData,
-                  description: text,
-                }))
-              }
-            />
-          </View>
-          <Dropdown
-            style={styles.dropdown}
-            placeholderStyle={styles.placeholderStyle}
-            selectedTextStyle={styles.selectedTextStyle}
-            iconStyle={styles.iconStyle}
-            data={data}
-            maxHeight={300}
-            labelField="label"
-            valueField="value"
-            placeholder="Choose day"
-            value={value}
-            onChange={(item) => {
-              setValue(item.value);
-            }}
-            renderItem={renderItem}
-          />
-          <ExerciseForm
-
-            onAddExercise={
-              value === "1"
-                ? addExerciseMonday
-                : value === "2"
-                ? addExerciseTuesday
-                : value === "3"
-                ? addExerciseWednesday
-                : value === "4"
-                ? addExerciseThursday
-                : value === "5"
-                ? addExerciseFriday
-                : value === "6"
-                ? addExerciseSaturday
-                : value === "7"
-                ? addExerciseSunday
-                : null
-            }
-          />
-
-          <TouchableOpacity style={styles.saveButton} onPress={saveWorkout}>
-            <Text
-              style={{
-                fontSize: 16,
-                color: "white",
-                textAlign: "center",
-                marginTop: 8,
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "undefined"}
+      >
+        <LinearGradient
+          style={styles.background}
+          colors={["#0F0264", "#0F3362", "#030D01"]}
+        />
+        <ScrollView style={styles.scrollview}>
+          <View style={styles.container}>
+            <View style={styles.formInputContainer}>
+              <TextInput
+                placeholder="Name"
+                placeholderTextColor="#C5C5CD"
+                style={styles.textInput}
+                value={workoutData.name}
+                autoCapitalize="none"
+                onChangeText={(text) =>
+                  setWorkoutData((prevData) => ({ ...prevData, name: text }))
+                }
+              />
+              <TextInput
+                placeholder="Description"
+                placeholderTextColor="#C5C5CD"
+                style={styles.textInput}
+                value={workoutData.description}
+                autoCapitalize="none"
+                onChangeText={(text) =>
+                  setWorkoutData((prevData) => ({
+                    ...prevData,
+                    description: text,
+                  }))
+                }
+              />
+            </View>
+            <Dropdown
+              style={styles.dropdown}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              iconStyle={styles.iconStyle}
+              data={data}
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              placeholder="Choose day"
+              value={value}
+              onChange={(item) => {
+                setValue(item.value);
               }}
+              renderItem={renderItem}
+            />
+            <ExerciseForm
+              onAddExercise={
+                value === "1"
+                  ? addExerciseMonday
+                  : value === "2"
+                  ? addExerciseTuesday
+                  : value === "3"
+                  ? addExerciseWednesday
+                  : value === "4"
+                  ? addExerciseThursday
+                  : value === "5"
+                  ? addExerciseFriday
+                  : value === "6"
+                  ? addExerciseSaturday
+                  : value === "7"
+                  ? addExerciseSunday
+                  : null
+              }
+            />
+            <TouchableOpacity
+              style={styles.saveButton}
+              onPress={handlePresentModal}
             >
-              Save
-            </Text>
-          </TouchableOpacity>
-          <BottomSheetModal
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: "white",
+                  textAlign: "center",
+                  marginTop: 8,
+                  
+                }}
+              >
+                Added Exercises
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.saveButton} onPress={saveWorkout}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: "white",
+                  textAlign: "center",
+                  marginTop: 8,
+                }}
+              >
+                Save
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+        <BottomSheetModal
           ref={bottomSheetModalRef}
           index={0}
           snapPoints={snapPoints}
           backdropComponent={renderBackdrop}
-        ></BottomSheetModal>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+        >
+          <View
+            style={{
+              justifyContent: "center",
+              marginTop: 20,
+              borderRadius: 20,
+              borderColor: "white",
+              borderWidth: 1,
+            }}
+          >
+            {workoutData.exercisesMonday.map((exercise) => (
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: 10,
+                  marginBottom: 7,
+                  width: "100%",
+                  paddingHorizontal: 30,
+                }}
+                key={exercise.id}
+              >
+                <Text style={{ width: "50%", fontSize: 20, color: "#C5C5CD" }}>
+                  {exercise.name}
+                </Text>
+                <View>
+                </View>
+              </View>
+            ))}
+                  <TouchableOpacity onPress={handleCloseModal}>
+                    <Text style={{ color: "red", fontSize: 20 }}>Cancel</Text>
+                  </TouchableOpacity>
+          </View>
+        </BottomSheetModal>
+      </KeyboardAvoidingView>
     </BottomSheetModalProvider>
   );
 }
@@ -254,7 +302,8 @@ const styles = StyleSheet.create({
     width: 150,
     backgroundColor: "#030266",
     borderRadius: 20,
-    
+    marginBottom: 10,
+    marginTop: 10
   },
   scrollview: {
     marginTop: 150,
@@ -273,12 +322,11 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.2,
     shadowRadius: 1.41,
-    
+
     elevation: 2,
   },
   icon: {
     marginRight: 5,
-    
   },
   item: {
     padding: 17,
@@ -290,11 +338,11 @@ const styles = StyleSheet.create({
   textItem: {
     flex: 1,
     fontSize: 16,
-    color: "white"
+    color: "white",
   },
   placeholderStyle: {
     fontSize: 16,
-    color: "white"
+    color: "white",
   },
   selectedTextStyle: {
     fontSize: 16,
